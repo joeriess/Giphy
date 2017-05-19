1b76c8d2f510c35c2d6a4e6325e3a259669c997b
Joe Riess <joeriess94@gmail.com>
2017-05-18 22:04:16 -0400

    Image type

---
+++ Giphy/Image.swift
@@ -0,0 +1,36 @@
+//
+//  Image.swift
+//  Giphy
+//
+//  Created by Joe Riess on 5/18/17.
+//  Copyright © 2017 Joe Riess. All rights reserved.
+//
+
+import Foundation
+
+public struct Image {
+    
+    /// URL of the image
+    public let url: URL
+    
+    ///
+    public let width: String
+    
+    ///
+    public let height: String
+    
+    ///
+    public let size: String
+    
+    ///
+    public let mp4: URL?
+    
+    ///
+    public let mp4Size: String?
+    
+    ///
+    public let webp: URL?
+    
+    ///
+    public let webpSize: String?
+}
