d1548fc9ae8e2666cb1e76923b04d3a0f6e24c74
Joe Riess <joeriess94@gmail.com>
2017-05-18 21:29:54 -0400

    Initial Commit

---
+++ Giphy/Giphy.h
@@ -0,0 +1,19 @@
+//
+//  Giphy.h
+//  Giphy
+//
+//  Created by Joe Riess on 5/18/17.
+//  Copyright © 2017 Joe Riess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Giphy.
+FOUNDATION_EXPORT double GiphyVersionNumber;
+
+//! Project version string for Giphy.
+FOUNDATION_EXPORT const unsigned char GiphyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Giphy/PublicHeader.h>
+
+
